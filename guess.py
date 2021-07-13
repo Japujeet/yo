3e4ee4fe96a98f5a31b604f19ff0ddca33c20030
Japujeet <72856439+Japujeet@users.noreply.github.com>
2021-07-13 17:23:15 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,13 @@
+n=int(10)
+g=int(input('Guess The Number I Chose For Youuuuuuuu!   Your Guess: '))
+
+if(g>n) :
+    print('less than')
+    print(g)
+
+if(g<n) :
+    print('greater than')
+    print(g)
+
+if(g==n):
+    print('Correct!')
